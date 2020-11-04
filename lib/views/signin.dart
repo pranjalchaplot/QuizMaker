943f65254babe36ea50cb33d64ed87d8de5fe20f
Pranjal Chaplot <pranjalchaplot@gmail.com>
2020-11-04 19:09:54 +0530

    completing signIn feature

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:main/services/auth.dart';
+import 'package:main/views/home.dart';
 import 'package:main/views/signup.dart';
 import 'package:main/widgets/widgets.dart';
 
@@ -14,9 +15,20 @@ class _SignInState extends State<SignIn> {
   String email, password;
   AuthService authService = new AuthService();
 
+  bool isLoading = false;
+
   signIn() async {
     if (_formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
       await authService.signInEmailAndPass(email, password);
+      setState(() {
+        isLoading = false;
+      });
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
     }
   }
 
@@ -31,93 +43,99 @@ class _SignInState extends State<SignIn> {
         elevation: 0.0,
         brightness: Brightness.light,
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              Spacer(),
-              TextFormField(
-                validator: (val) {
-                  return RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(val)
-                      ? null
-                      : "Enter correct email";
-                },
-                decoration: InputDecoration(hintText: "Email"),
-                onChanged: (val) {
-                  email = val;
-                },
+      body: isLoading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
-              SizedBox(height: 6),
-              TextFormField(
-                validator: (val) {
-                  return val.length < 6
-                      ? "Please provide with 6+ character"
-                      : null;
-                },
-                decoration: InputDecoration(hintText: "Password"),
-                onChanged: (val) {
-                  password = val;
-                },
-              ),
-              SizedBox(height: 24),
-              GestureDetector(
-                onTap: () {
-                  signIn();
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 18),
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width - 48,
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+            )
+          : Form(
+              key: _formKey,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                    Spacer(),
+                    TextFormField(
+                      validator: (val) {
+                        return RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(val)
+                            ? null
+                            : "Enter correct email";
+                      },
+                      decoration: InputDecoration(hintText: "Email"),
+                      onChanged: (val) {
+                        email = val;
+                      },
                     ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 18),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account? ",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUp(),
-                          ));
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        fontSize: 16,
-                        decoration: TextDecoration.underline,
+                    SizedBox(height: 6),
+                    TextFormField(
+                      validator: (val) {
+                        return val.length < 6
+                            ? "Please provide with 6+ character"
+                            : null;
+                      },
+                      decoration: InputDecoration(hintText: "Password"),
+                      onChanged: (val) {
+                        password = val;
+                      },
+                    ),
+                    SizedBox(height: 24),
+                    GestureDetector(
+                      onTap: () {
+                        signIn();
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 18),
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 48,
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 18),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account? ",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignUp(),
+                                ));
+                          },
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontSize: 16,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                    )
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 80,
-              )
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
